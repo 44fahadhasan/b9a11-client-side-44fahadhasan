2d402f4431a6a762a8a8ce5d2cbdbd3494dbf01a
Fahad Hasan <44fahadhasan@gmail.com>
2024-05-10 15:43:26 +0600

    added navbar

--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -1,8 +1,10 @@
+import NavBar from "../../shared/NabBar/NavBar";
 import ThemeController from "../ThemeController/ThemeController";
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <ThemeController />
     </div>
   );
