48a875a47fc87a89b56157709d042967cdfd70d3
Fahad Hasan <44fahadhasan@gmail.com>
2024-05-10 14:02:58 +0600

    some folder structure setup for project and added dark and light theme

---
+++ src/pages/HomePage/Home/Home.jsx
@@ -0,0 +1,11 @@
+import ThemeController from "../ThemeController/ThemeController";
+
+const Home = () => {
+  return (
+    <div>
+      <ThemeController />
+    </div>
+  );
+};
+
+export default Home;
