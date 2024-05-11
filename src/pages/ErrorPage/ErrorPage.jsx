086247f4a676d9fe5332befaf7f19368c8ae2b77
Fahad Hasan <44fahadhasan@gmail.com>
2024-05-11 19:26:28 +0600

    added error page design

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,59 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <section className="poppins flex justify-center items-center h-screen p-16 bg-base-100 text-base-content">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-6xl  md:text-7xl xl:text-9xl text-gray-600">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Sorry, we couldn&apos;t find this page.
+          </p>
+          <div className="animate-bounce pt-9 pb-4">
+            <svg
+              className="mx-auto h-16 w-16 text-[#ff1949]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+              ></path>
+            </svg>
+          </div>
+
+          <button
+            onClick={() => navigate("/")}
+            type="button"
+            className="btn btn-primary inline-flex items-center gap-x-2 font-bold"
+          >
+            Back Home
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
