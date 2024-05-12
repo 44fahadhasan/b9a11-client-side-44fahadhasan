5ee1512b955d084970950d90cddb4b5c878975ac
Fahad Hasan <44fahadhasan@gmail.com>
2024-05-12 23:12:31 +0600

    added AssignmentCard component and Assignments page some work done

--- src/pages/AssignmentsPage/AssignmentsPage.jsx
+++ src/pages/AssignmentsPage/AssignmentsPage.jsx
@@ -1,9 +1,32 @@
-// import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
+import useAxiosURL from "../../hooks/useAxiosURL";
+import AssignmentCard from "../shared/AssignmentCard/AssignmentCard";
 
 const AssignmentsPage = () => {
-  return <div>AssignmentsPage</div>;
-};
+  const [assignments, setAssignments] = useState([]);
 
-AssignmentsPage.propTypes = {};
+  const axiosOpenURL = useAxiosURL();
+
+  useEffect(() => {
+    axiosOpenURL
+      .get("/assignments")
+      .then((response) => {
+        setAssignments(response?.data);
+      })
+      .catch((error) => {
+        console.error(error?.message);
+      });
+  }, [axiosOpenURL]);
+
+  return (
+    <section className="poppins flex flex-col items-center bg-base-100">
+      <div className="mt-10 grid max-w-md grid-cols-1 gap-6 px-2 sm:max-w-lg sm:px-20 md:max-w-screen-xl md:grid-cols-2 md:px-10 lg:grid-cols-3 lg:gap-8">
+        {assignments.map((assignment) => (
+          <AssignmentCard key={assignment?._id} assignment={assignment} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default AssignmentsPage;
---
+++ src/pages/shared/AssignmentCard/AssignmentCard.jsx
@@ -0,0 +1,87 @@
+import PropTypes from "prop-types";
+import { GrView } from "react-icons/gr";
+import { MdDateRange, MdDeleteForever, MdEditSquare } from "react-icons/md";
+
+const AssignmentCard = ({
+  assignment: {
+    _id,
+    title,
+    description,
+    marks,
+    thumbnailImageUrl,
+    level,
+    dueDate,
+  } = {},
+}) => {
+  return (
+    <article className="mb-4 group overflow-hidden rounded-xl border shadow-md duration-500 ease-in-out hover:shadow-xl">
+      {/* card top */}
+      <div className="relative overflow-hidden">
+        {/* img */}
+        <div>
+          <img
+            className="w-full object-cover"
+            src={
+              thumbnailImageUrl &&
+              "https://i.ibb.co/0ckKbwn/240-F-309598037-Frf-MLAf-TQrsts-Vg5-SZMa-GH1-Uef6s69-EB.jpg"
+            }
+          />
+        </div>
+
+        {/* img opacity */}
+        <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
+
+        {/* level */}
+        <div className="absolute bottom-0 left-0 bg-primary px-3 py-2 text-white rounded-tr-lg text-sm">
+          {level}
+        </div>
+
+        {/* marks */}
+        <div className="text-sm absolute top-0 left-0 bg-primary px-4 text-white rounded-full h-14 w-14 flex flex-col items-center justify-center mt-1 ml-1">
+          <span className="font-bold">{marks}</span>
+          <small className="font-medium">Marks</small>
+        </div>
+
+        {/* delete,update,view button */}
+        <div className="absolute -right-16 bottom-0 mr-2 mb-4 space-y-2 transition-all duration-300 group-hover:right-0">
+          <button className="flex items-center justify-center h-10 w-10 bg-primary rounded-full text-white">
+            <MdDeleteForever className="h-6 w-6" />
+          </button>
+          <button className="flex items-center justify-center h-10 w-10 bg-primary rounded-full text-white">
+            <MdEditSquare className="h-5 w-5" />
+          </button>
+          <button className="flex items-center justify-center h-10 w-10 bg-primary rounded-full text-white">
+            <GrView className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+
+      {/* card bottom */}
+      <div className="p-6">
+        <div className="pb-1">
+          <h2 className="text-[22px] font-bold">{title?.slice(0, 25)}</h2>
+        </div>
+
+        <p className="opensans text-[15px]">{description?.slice(0, 70)} ...</p>
+
+        <div className="pt-4 flex flex-row items-center">
+          <span
+            href="#"
+            className="py-1 text-[15px] font-regular  mr-1 flex flex-row items-center"
+          >
+            <MdDateRange className="text-base  text-primary" />
+            <span className="ml-1">
+              {new Date(dueDate).toLocaleDateString()}
+            </span>
+          </span>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+AssignmentCard.propTypes = {
+  assignment: PropTypes.object,
+};
+
+export default AssignmentCard;
