bcb06ba9b1c93521e03d1e70b1ab859e4f476d59
Fahad Hasan <44fahadhasan@gmail.com>
2024-05-15 23:41:48 +0600

    title dynamicaly updated

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,8 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/shared/Footer/Footer";
 import NavBar from "../pages/shared/NabBar/NavBar";
 
 const Root = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = "Raque - Home";
+      return;
+    }
+    document.title = `Raque - ${location.pathname
+      .replaceAll("/", " - ")
+      .replaceAll("-", " ")}`;
+  }, [location]);
+
   return (
     <div>
       <NavBar />
